5e441288c0fc02b7738a483bdbdf5b819a63924f
Jean Lopez <jeanlopezcortes@gmail.com>
2024-08-12 02:34:26 -0400

    mediaType enum fixed

--- src/mongoose/user.schema.ts
+++ src/mongoose/user.schema.ts
@@ -1,7 +1,24 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
-import { HydratedDocument } from 'mongoose';
+import { HydratedDocument, Types } from 'mongoose';
 
 export type UserDocument = HydratedDocument<User>;
+
+@Schema()
+export class Media {
+  @Prop({ required: true })
+  mediaId: string; // The ID of the media in the corresponding media API
+
+  @Prop({ 
+    required: true,
+    enum: ['MOVIE', 'SERIE'] // Enum to restrict values
+  })
+  mediaType: string; // The type of the media in the corresponding media API ej: mediaType=Movie
+
+  @Prop({ required: true })
+  seen: boolean;
+}
+
+export const MediaSchema = SchemaFactory.createForClass(Media);
 
 @Schema()
 export class User {
@@ -14,24 +31,8 @@ export class User {
   @Prop({ required: true })
   password: string;
 
-  @Prop({ default: [] })
-  mediaList: [Media];
-
-}
-
-export class Media {
-  @Prop({ required: true })
-  mediaId: string; // The ID of the media in the corresponding media API
-
-  @Prop({ 
-    required: true,
-    enum: ['MOVIE', 'SERIE']
-  })
-  mediaType: string;// The type of the media in the corresponding media API ej: mediaType=Movie
-
-  @Prop({ required: true })
-  seen: boolean;
-
+  @Prop({ type: [MediaSchema], default: [] }) // Explicitly defining the type as an array of MediaSchema
+  mediaList: Media[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
